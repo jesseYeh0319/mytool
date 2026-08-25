64a97b6efab99e088c6989ce497daa9ef1d27fcc
jesse <yehjesse96@gmail.com>
2026-08-25 11:46:44 +0800

    Use native SQLite for Nuxt Content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-  ],
+  modules: ['@nuxt/content'],
+
+  content: {
+    experimental: {
+      sqliteConnector: 'native',
+    },
+  },
+
   devtools: { enabled: true },
-  compatibilityDate: '2024-04-03',
 })
