4d7039b6772861cadc43cbc2d5961df603312ed3
jesse <yehjesse96@gmail.com>
2026-09-03 18:14:52 +0800

    Configure Supabase runtime settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,63 +1,70 @@
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-    '@nuxtjs/sitemap',
-    '@nuxtjs/robots',
-  ],
-
-  site: {
-    url: 'https://mytool-mybb.vercel.app',
-    name: 'MYBB',
-  },
-
-  sitemap: {
-    sources: [
-      '/api/__sitemap__/urls',
+    modules: [
+        '@nuxt/content',
+        '@nuxtjs/sitemap',
+        '@nuxtjs/robots',
     ],
-  },
 
-  content: {
-    experimental: {
-      sqliteConnector: 'native',
+    site: {
+        url: 'https://mytool-mybb.vercel.app',
+        name: 'MYBB',
     },
 
-    build: {
-      markdown: {
-        highlight: {
-          theme: 'github-dark',
+    sitemap: {
+        sources: [
+            '/api/__sitemap__/urls',
+        ],
+    },
 
-          langs: [
-            'java',
-            'sql',
-            'js',
-            'ts',
-            'vue',
-            'html',
-            'css',
-            'json',
-            'yaml',
-            'shell',
-            'bash',
-          ],
+    content: {
+        experimental: {
+            sqliteConnector: 'native',
         },
-      },
-    },
-  },
 
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: 'zh-TW',
-      },
+        build: {
+            markdown: {
+                highlight: {
+                    theme: 'github-dark',
 
-      meta: [
-        {
-          name: 'author',
-          content: 'MYBB',
+                    langs: [
+                        'java',
+                        'sql',
+                        'js',
+                        'ts',
+                        'vue',
+                        'html',
+                        'css',
+                        'json',
+                        'yaml',
+                        'shell',
+                        'bash',
+                    ],
+                },
+            },
         },
-      ],
     },
-  },
 
-  devtools: { enabled: true },
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: 'zh-TW',
+            },
+
+            meta: [
+                {
+                    name: 'author',
+                    content: 'MYBB',
+                },
+            ],
+        },
+    },
+
+    runtimeConfig: {
+        public: {
+            supabaseUrl: '',
+            supabaseKey: '',
+        },
+    },
+
+    devtools: { enabled: true },
 })
